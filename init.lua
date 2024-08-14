d9d88550f35a26b688801f68b2c5e6742f7b7065
Addison Fischer <addisonstevenfischer@gmail.com>
2024-08-14 11:01:32 -0400

    Codeium install 08/14/24

--- init.lua
+++ init.lua
@@ -226,7 +226,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
+  'Exafunction/codeium.vim', -- codeium install 08/14/2024
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -927,10 +927,9 @@ require('lazy').setup({
   },
 })
 
---Custom entries from me!
 vim.keymap.set('n', '<leader>e', ':e <C-r>=expand("%:p:h")<CR>')
 vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'NONE' })
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE' })
-require 'ThePrimeagen/vim-be-good'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
