5ea2d1e786b4fdc0041b4d5bde3afb27f4db8542
Addison Fischer <addisonstevenfischer@gmail.com>
2024-12-19 23:00:06 -0500

    Edited everything, added floating terminal/tt, custom commands for cloudsync, xml/F# addons, and proper terminal

--- init.lua
+++ init.lua
@@ -1,88 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -225,6 +140,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'pamoller/xmlformatter',
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'Exafunction/codeium.vim', -- codeium install 08/14/2024
   'ThePrimeagen/harpoon', -- harpoon install 08/18/2024
@@ -282,6 +198,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
+        { '<leader>tt', group = '[T]oggle [T]erminal' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       }
     end,
@@ -860,6 +777,8 @@ require('lazy').setup({
         'json',
         'yaml',
         'toml',
+        'xml',
+        'fsharp',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
@@ -899,19 +818,19 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'custom.plugins.harpoon',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
@@ -937,6 +856,90 @@ require('lazy').setup({
 vim.keymap.set('n', '<leader>e', ':e <C-r>=expand("%:p:h")<CR>')
 vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'NONE' })
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE' })
+--Custom minify xml
+vim.api.nvim_create_user_command('RWS', function()
+  vim.cmd [[
+    %s/[ \t\r\n]\+//g
+]]
+end, { desc = 'Minify XML by removing whitespace' })
 
+function ReplacePrefix(old_prefix, new_prefix)
+  vim.cmd(string.format('%%s/%s\\.(%s)/{{%s_\\1}}/g', old_prefix, '%w+', new_prefix))
+end
+
+vim.api.nvim_create_user_command('RCS', function(opts)
+  local args = vim.split(opts.args, ' ')
+  if #args ~= 2 then
+    print 'Usage: RCS <word_to_replace> <replacement>'
+    return
+  end
+
+  local word_to_replace = args[1]
+  local replacement = args[2]
+
+  -- Proper escaping of backreference
+  local cmd = string.format('%%s/%s\\.\\(\\w\\+\\)/{{%s_\\1}}/g', word_to_replace, replacement)
+  vim.cmd(cmd)
+end, { nargs = '+' })
+
+vim.g.zip_update_cmd = '7z u -tzip %s %s'
+
+-- Function to format XML
+local function format_xml()
+  local start_line, start_col = unpack(vim.fn.getpos "'<", 2, 3)
+  local end_line, end_col = unpack(vim.fn.getpos "'>", 2, 3)
+  local range = start_line ~= end_line or start_col ~= end_col
+
+  if range then
+    -- Format selected text
+    vim.cmd "silent! '<,'>!xmllint --format -"
+  else
+    -- Format each XML document separately
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    local formatted_lines = {}
+    local xml_chunk = ''
+
+    for _, line in ipairs(lines) do
+      if line:match '^<%?xml' or line:match '^<note' then
+        if #xml_chunk > 0 then
+          local handle = io.popen("echo '" .. xml_chunk .. "' | xmllint --format -")
+          local result = handle:read '*a'
+          handle:close()
+          for formatted_line in result:gmatch '[^\r\n]+' do
+            table.insert(formatted_lines, formatted_line)
+          end
+          xml_chunk = ''
+        end
+      end
+      xml_chunk = xml_chunk .. line
+    end
+
+    if #xml_chunk > 0 then
+      local handle = io.popen("echo '" .. xml_chunk .. "' | xmllint --format -")
+      local result = handle:read '*a'
+      handle:close()
+      for formatted_line in result:gmatch '[^\r\n]+' do
+        table.insert(formatted_lines, formatted_line)
+      end
+    end
+
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, formatted_lines)
+  end
+end
+
+-- Create the :FXML command
+vim.api.nvim_create_user_command('FXML', format_xml, { range = true })
+vim.o.shell = 'pwsh.exe'
+
+vim.o.shellcmdflag =
+  '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+
+vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+
+vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+
+vim.o.shellquote = ''
+
+vim.o.shellxquote = ''
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
